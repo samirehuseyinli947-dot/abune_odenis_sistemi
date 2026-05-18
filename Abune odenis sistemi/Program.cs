5022c2fbc4da74b49b95477a2cd49012ec3826e5
samirehuseyinli947-dot <samirehuseyinli947@gmail.com>
2026-05-18 12:09:35 +0400

    Implement user age check and list display
    Added user age validation and user list display functionality.

--- Abune odenis sistemi/Program.cs
+++ Abune odenis sistemi/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,8 @@ namespace Abune_odenis_sistemi
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 1; i++)
+            List<User> users = new List<User>();
+            for (int i = 0; i < 3; i++)
             {
                 User u1 = new User();
                 Console.WriteLine(" Adinizi daxil edin:");
@@ -23,10 +24,32 @@ namespace Abune_odenis_sistemi
                 u1.phone = Console.ReadLine();
                 Console.WriteLine("Yashinizi daxil edin:");
                 u1.age = int.Parse(Console.ReadLine());
+                if (u1.age >= 18)
+                { 
                 Console.WriteLine("Parolunuzu daxil edin:");
                 u1.password = Console.ReadLine();
-                Console.WriteLine(u1.name + " " + u1.surname);
+                
+                users.Add(u1);
 
+                Console.WriteLine("User ugurla elave edildi");
+                Console.WriteLine(u1.name + " " + u1.surname);
+                }
+                else
+                {
+                    Console.WriteLine("Abune olmaq ucun yasin 18 ve ya daha boyuk olmalidir.");
+                }
+                Console.WriteLine("\n===== LISTDEKI USERLER =====");
+
+                foreach (User user in users)
+                {
+                    Console.WriteLine("Ad: " + user.name);
+                    Console.WriteLine("Soyad: " + user.surname);
+                    Console.WriteLine("Email: " + user.email);
+                    Console.WriteLine("Telefon: " + user.phone);
+                    Console.WriteLine("Yas: " + user.age);
+
+                    Console.WriteLine("--------------------------");
+                }
             }
             Admin admin1 = new Admin();
             admin1.Id = 1;
